916216acbf998379dcf5f0dd0cc920ba330a7007
Don Petersen <don.petersen@g5searchmarketing.com>
2013-02-04 08:38:52 -0800

    Fix intermittent rbenv issue.
    Sometimes (about 50/50) the vagrant user doesn't get added to the rbenv group.
    No idea why.  Hopefully this fixes.

--- vagrant_rbenv/recipes/default.rb
+++ vagrant_rbenv/recipes/default.rb
@@ -1,7 +1,13 @@
 include_recipe "rbenv::default"
 include_recipe "rbenv::ruby_build"
 
+# Should probably fix the apparently busted rbenv recipe.
 execute "Set rbenv directory group permissions" do
   command "chmod -R 775 /opt/rbenv"
   action :run
+end
+
+# Should probably fix the apparently busted rbenv recipe.
+execute "Add vagrant user to the rbenv group" do
+  command "sudo usermod -a -G rbenv vagrant"
 end
