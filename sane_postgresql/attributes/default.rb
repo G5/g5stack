41bd655ed696ef459e77a63192f19a80b6be2d6c
maeve <maeve.revels@getg5.com>
2014-02-10 00:24:37 -0800

    Override postgresql defaults to install 9.3

--- sane_postgresql/attributes/default.rb
+++ sane_postgresql/attributes/default.rb
@@ -1,6 +1,8 @@
+set[:postgresql][:version] = '9.3'
+set[:postgresql][:enable_pgdg_apt] = true
+
 include_attribute 'postgresql'
 
-default[:postgresql][:version] = '9.3'
 default[:postgresql][:pg_hba] << {
   :type => 'local',
   :db => 'all',
--- sane_postgresql/recipes/default.rb
+++ sane_postgresql/recipes/default.rb
@@ -4,21 +4,22 @@ template "/etc/default/locale" do
   mode "0644"
 end
 
+include_recipe "postgresql::apt_pgdg_postgresql"
 include_recipe "postgresql::server"
 
-execute "tear down postgres to rebuild it with UTF-8 encoding by default" do
-  user "root"
-  command %{
-    touch /home/vagrant/.encoding-fixed
-    cp /etc/postgresql/9.1/main/pg_hba.conf /tmp/
-    cp /etc/postgresql/9.1/main/postgresql.conf /tmp/
-    pg_dropcluster --stop 9.1 main
-    pg_createcluster --start -e UTF-8 9.1 main
-    cp /tmp/pg_hba.conf /etc/postgresql/9.1/main/
-    cp /tmp/postgresql.conf /etc/postgresql/9.1/main/
-    /etc/init.d/postgresql restart
-    sudo -u postgres psql -c "CREATE USER vagrant CREATEDB SUPERUSER"
-  }
+# execute "tear down postgres to rebuild it with UTF-8 encoding by default" do
+  # user "root"
+  # command %{
+    # touch /home/vagrant/.encoding-fixed
+    # cp /etc/postgresql/9.1/main/pg_hba.conf /tmp/
+    # cp /etc/postgresql/9.1/main/postgresql.conf /tmp/
+    # pg_dropcluster --stop 9.1 main
+    # pg_createcluster --start -e UTF-8 9.1 main
+    # cp /tmp/pg_hba.conf /etc/postgresql/9.1/main/
+    # cp /tmp/postgresql.conf /etc/postgresql/9.1/main/
+    # /etc/init.d/postgresql restart
+    # sudo -u postgres psql -c "CREATE USER vagrant CREATEDB SUPERUSER"
+  # }
 
-  not_if { File.exist?("/home/vagrant/.encoding-fixed") }
-end
+  # not_if { File.exist?("/home/vagrant/.encoding-fixed") }
+# end
