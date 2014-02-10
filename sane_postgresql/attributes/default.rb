f6a1c4644e7ef512a55e787cc1f6eeeac0f1da08
maeve <maeve.revels@getg5.com>
2014-02-10 00:35:58 -0800

    Force config syntax specific to postgresql 9.3

--- sane_postgresql/attributes/default.rb
+++ sane_postgresql/attributes/default.rb
@@ -6,6 +6,12 @@ include_attribute 'postgresql'
 default[:postgresql][:config][:ssl_key_file]  = '/etc/ssl/private/ssl-cert-snakeoil.key'
 default[:postgresql][:config][:ssl_cert_file] = '/etc/ssl/certs/ssl-cert-snakeoil.pem'
 
+# The postgresql recipe has a tricky bug where it doesn't consistently
+# use the 9.3 config syntax, due to the fact that it casts the version
+# string to a float for comparison.
+default[:postgresql][:config].delete(:unix_socket_directory)
+default[:postgresql][:config][:unix_socket_directories] = '/var/run/postgresql'
+
 default[:postgresql][:pg_hba] << {
   :type => 'local',
   :db => 'all',
