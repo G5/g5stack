62ffd40b6dc5c2c471704940bbd840ada0112075
maeve <maeve.revels@getg5.com>
2014-02-10 00:32:34 -0800

    Set postgresql default ssl cert

--- sane_postgresql/attributes/default.rb
+++ sane_postgresql/attributes/default.rb
@@ -3,6 +3,9 @@ set[:postgresql][:enable_pgdg_apt] = true
 
 include_attribute 'postgresql'
 
+default[:postgresql][:config][:ssl_key_file]  = '/etc/ssl/private/ssl-cert-snakeoil.key'
+default[:postgresql][:config][:ssl_cert_file] = '/etc/ssl/certs/ssl-cert-snakeoil.pem'
+
 default[:postgresql][:pg_hba] << {
   :type => 'local',
   :db => 'all',
