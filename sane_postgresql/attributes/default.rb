4baa2f3da16a7b59fde8098b84bb3ad8189c5a75
maeve <maeve.revels@getg5.com>
2014-02-09 23:08:49 -0800

    Force postgresql attributes to load before sane_postgresql

--- sane_postgresql/attributes/default.rb
+++ sane_postgresql/attributes/default.rb
@@ -1,4 +1,6 @@
-default[:postgresql][:version] = "9.1"
+include_attribute 'postgresql'
+
+default[:postgresql][:version] = '9.3'
 default[:postgresql][:pg_hba] << {
   :type => 'local',
   :db => 'all',
