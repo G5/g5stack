092dbe5d29578e7661ff952d9179ea68e0ae6098
maeve <maeve.revels@getg5.com>
2014-02-10 01:58:57 -0800

    Remove obsolete locale workaround

--- sane_postgresql/recipes/default.rb
+++ sane_postgresql/recipes/default.rb
@@ -1,25 +1,11 @@
-# Intended for postgres, who completely ignores it, but still a good idea.
-template "/etc/default/locale" do
-  source "locale"
-  mode "0644"
+template '/etc/default/locale' do
+  source 'locale'
+  mode '0644'
 end
 
-include_recipe "postgresql::apt_pgdg_postgresql"
-include_recipe "postgresql::server"
+include_recipe 'postgresql::apt_pgdg_postgresql'
+include_recipe 'postgresql::server'
 
-# execute "tear down postgres to rebuild it with UTF-8 encoding by default" do
-  # user "root"
-  # command %{
-    # touch /home/vagrant/.encoding-fixed
-    # cp /etc/postgresql/9.1/main/pg_hba.conf /tmp/
-    # cp /etc/postgresql/9.1/main/postgresql.conf /tmp/
-    # pg_dropcluster --stop 9.1 main
-    # pg_createcluster --start -e UTF-8 9.1 main
-    # cp /tmp/pg_hba.conf /etc/postgresql/9.1/main/
-    # cp /tmp/postgresql.conf /etc/postgresql/9.1/main/
-    # /etc/init.d/postgresql restart
-    # sudo -u postgres psql -c "CREATE USER vagrant CREATEDB SUPERUSER"
-  # }
-
-  # not_if { File.exist?("/home/vagrant/.encoding-fixed") }
-# end
+execute 'Create a vagrant db user' do
+  command 'sudo -u postgres psql -c "CREATE USER vagrant CREATEDB SUPERUSER"'
+end
