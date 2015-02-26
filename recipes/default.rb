590702f0bfe155982dda55a13cc0d5f4d8a44fe7
maeve <maeve.revels@getg5.com>
2015-02-26 14:13:40 -0800

    Install imagemagick

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,7 @@ include_recipe 'g5-nodejs'
 include_recipe 'phantomjs'
 include_recipe 'redisio'
 include_recipe 'redisio::enable'
+include_recipe 'imagemagick::devel'
 
 [ 'vim', 'libsqlite3-dev', 'sqlite3'].each do |package_name|
   package package_name
